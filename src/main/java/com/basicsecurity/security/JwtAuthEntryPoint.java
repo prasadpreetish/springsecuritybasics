a772669bd4c0b5a5dda872550a8eff6717f8bf50
Preetish Prasad <www.preetishpd@gmail.com>
2025-06-28 12:56:56 +0530

    Commit-6 Added JwtAuthEntryPoint. This class will implement Spring Security's AuthenticationEntryPoint interface and will be triggered when an unauthenticated user attempts to access a protected resource. Instead of redirecting to a login page (which is typical for traditional web apps), we'll send an HTTP 401 Unauthorized response.

---
+++ src/main/java/com/basicsecurity/security/JwtAuthEntryPoint.java
@@ -0,0 +1,22 @@
+package com.basicsecurity.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        // This method is called whenever an unauthenticated user tries to access a protected resource.
+        // We send an HTTP 401 Unauthorized response with a message.
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+    }
+}
